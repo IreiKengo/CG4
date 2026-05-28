a3515721d18e738a35a7e785e6cb631e79fcbf5a
Kengo Irei <kenyougo04@icloud.com>
2026-05-28 12:59:14 +0900

    DXGI_FORMAT_R32G32_FLOATをDXGI_FORMAT_R32G32B32_FLOATに修正

--- SkyboxCommon.cpp
+++ SkyboxCommon.cpp
@@ -116,7 +116,7 @@ void SkyboxCommon::CreateGraphicsPipeline()
 	inputElementDescs[0].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
 	inputElementDescs[1].SemanticName = "TEXCOORD";
 	inputElementDescs[1].SemanticIndex = 0;
-	inputElementDescs[1].Format = DXGI_FORMAT_R32G32_FLOAT;
+	inputElementDescs[1].Format = DXGI_FORMAT_R32G32B32_FLOAT;
 	inputElementDescs[1].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;;
 	D3D12_INPUT_LAYOUT_DESC inputLayoutDesc{};
 	inputLayoutDesc.pInputElementDescs = inputElementDescs;
